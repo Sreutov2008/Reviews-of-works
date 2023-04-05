c9a157f1662553ad77a125cb5206c386cbd980c0
Александр Реутов <sreutov2008@yandex.ru>
2023-04-05 11:29:15 +0700

    version 1.4

--- api_yamdb/reviews/management/commands/import_csv.py
+++ api_yamdb/reviews/management/commands/import_csv.py
@@ -3,7 +3,7 @@ from csv import DictReader
 
 from django.contrib.auth import get_user_model
 from django.core.management.base import BaseCommand
-from reviews.models import Category, Comment, Genre, GenreTitle, Review, Title
+from api_yamdb.reviews.models import Category, Comment, Genre, GenreTitle, Review, Title
 
 User = get_user_model()
 
